d7fdd63263426eaeeea325b8e7e122d3dec9b753
Alexander Serheyev <alex@dna.tech>
2021-06-08 11:34:43 +0400

    Bash install script

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,13 +1,13 @@
 set -o vi
 bind -m vi-insert "\C-l":clear-screen
 
-#source $HOME/.bash/arch.sh
-source $HOME/.bash/macos.sh
-source $HOME/.bash/history.sh
-source $HOME/.bash/common.sh
-source $HOME/.bash/docker.sh
-source $HOME/.bash/git.sh
-source $HOME/.bash/golang.sh
-source $HOME/.bash/composer.sh
-source $HOME/.bash/symfony.sh
-source $HOME/.bash/tmux.sh
+#source $HOME/dotfiles/bash/arch.sh
+source $HOME/dotfiles/bash/macos.sh
+source $HOME/dotfiles/bash/history.sh
+source $HOME/dotfiles/bash/common.sh
+source $HOME/dotfiles/bash/docker.sh
+source $HOME/dotfiles/bash/git.sh
+source $HOME/dotfiles/bash/golang.sh
+source $HOME/dotfiles/bash/composer.sh
+source $HOME/dotfiles/bash/symfony.sh
+source $HOME/dotfiles/bash/tmux.sh
