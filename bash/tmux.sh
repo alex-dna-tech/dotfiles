a4cc34d1b20a28e5f9663d5c163caaa159cc7811
Alex Serheyev <alex@dna.tech>
2022-07-26 06:33:22 +0300

    🚚chore(bash): move code and fix tmux attach

--- bash/tmux.sh
+++ bash/tmux.sh
@@ -4,3 +4,11 @@ alias ts='tmux new-session -s'
 alias tl='tmux list-sessions'
 alias tksv='tmux kill-server'
 alias tkss='tmux kill-session -t'
+
+tmux has-session 2> /dev/null
+if [ $? -ne 0 ]; then
+  TMUX='' tmux new-session -s $(basename ${PWD})
+else
+  [ -z "$TMUX" ] && [ -z "$MYVIMRC" ] && tmux attach
+fi
+
