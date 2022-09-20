66f9e58a36e7f19057fa9542fecafaf9c23a4b4c
Alex Serheyev <alex@dna.tech>
2022-09-20 08:28:10 +0300

    ✨feat(nvim): ansible support

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -79,6 +79,7 @@ return packer.startup(function(use)
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+  use "pearofducks/ansible-vim" -- ansible syntax
   use "SirVer/ultisnips"
 
   -- LSP
