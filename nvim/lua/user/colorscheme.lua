948834159610da03c7ba15438cf0a88578fb6b17
Alex Serheyev <alex@dna.tech>
2022-11-10 20:42:18 +0200

    🐛fix(nvim): vim console tranparent background

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,32 +1,15 @@
 vim.o.background = "dark"
 
-require("gruvbox").setup({
-  undercurl = true,
-  underline = true,
-  bold = true,
-  italic = true,
-  strikethrough = true,
-  invert_selection = false,
-  invert_signs = false,
-  invert_tabline = false,
-  invert_intend_guides = false,
-  inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "", -- can be "hard", "soft" or empty string
-  palette_overrides = {},
-  overrides = {},
-  dim_inactive = false,
-  transparent_mode = true,
-})
-
 vim.cmd [[
 try
   colorscheme gruvbox
+  autocmd VimEnter * hi Normal ctermbg=NONE guibg=NONE
+  let g:gruvbox_transparent_bg = 1
 
   set guicursor=n-v-c:block,i-ci-ve:ver100,r-cr:hor20,o:hor50
     \,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor
     \,sm:block-blinkwait175-blinkoff150-blinkon175
 catch /^Vim\%((\a\+)\)\=:E185/
-  set background=dark
   colorscheme default
 endtry
 ]]
