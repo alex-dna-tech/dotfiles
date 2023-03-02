1ed10564f8e30282b5a7222b4fee8d614a52d1d0
Alex Serheyev <alex@dna.tech>
2023-03-02 06:22:36 +0200

    🩹fix(nvim): fix format command

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -2,7 +2,7 @@ local lsp = require('lsp-zero')
 lsp.preset("recommended")
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
@@ -31,7 +31,7 @@ lsp.on_attach(function(_, bufnr)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next({ border = "rounded" }) end, opts)
   vim.keymap.set("n", "gl", function() vim.diagnostic.open_float({ border = "rounded" }) end, opts)
   vim.keymap.set("n", "<leader>q", function() vim.diagnostic.setloclist() end, opts)
-  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting( ]]
+  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.format()' ]]
 
   vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
