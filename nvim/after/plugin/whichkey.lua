275ec8a0b225aa54b784e89289372f2442acf1c3
Alex Serheyev <alex@dna.tech>
2023-04-27 21:35:53 +0300

    ⚰️chore(nvim): remove terminal from whichkey

--- nvim/after/plugin/whichkey.lua
+++ nvim/after/plugin/whichkey.lua
@@ -144,17 +144,6 @@ local mappings = {
     ["se"] = { "<cmd>lua require'telescope.builtin'.symbols{ sources = {'emoji'} }<cr>", "Emoji" },
     ["sg"] = { "<cmd>lua require'telescope.builtin'.symbols{ sources = {'gitmoji'} }<cr>", "Gitmoji" },
   },
-  t = {
-    name = "Terminal",
-    n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
-    u = { "<cmd>lua _NCDU_TOGGLE()<cr>", "NCDU" },
-    t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
-    p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
-    a = { "<cmd>lua _AIR_TOGGLE()<cr>", "Air" },
-    f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
-    h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
-    v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
-  },
 }
 
 local wk = require("which-key")
