294701099574548036a87e76d88c0dd89fb77e6c
Alex Serheyev <alex@dna.tech>
2023-04-27 12:19:53 +0300

    ⚡️refactor(nvim): remove toggleterm

--- nvim/lua/sim/packer.lua
+++ nvim/lua/sim/packer.lua
@@ -35,7 +35,6 @@ return require('packer').startup(function(use)
   use "nvim-telescope/telescope-symbols.nvim"
   use "folke/which-key.nvim"
   use { "numtostr/BufOnly.nvim", cmd = { "BufOnly" } }
-  use "akinsho/toggleterm.nvim"
   use "ahmedkhalf/project.nvim"
 
   -- Edition
@@ -56,20 +55,20 @@ return require('packer').startup(function(use)
     branch = 'v1.x',
     requires = {
       -- LSP Support
-      { 'neovim/nvim-lspconfig' },             -- Required
-      { 'williamboman/mason.nvim' },           -- Optional
+      { 'neovim/nvim-lspconfig' }, -- Required
+      { 'williamboman/mason.nvim' }, -- Optional
       { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
       -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },         -- Required
-      { 'hrsh7th/cmp-nvim-lsp' },     -- Required
-      { 'hrsh7th/cmp-buffer' },       -- Optional
-      { 'hrsh7th/cmp-path' },         -- Optional
+      { 'hrsh7th/nvim-cmp' }, -- Required
+      { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+      { 'hrsh7th/cmp-buffer' }, -- Optional
+      { 'hrsh7th/cmp-path' }, -- Optional
       { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-      { 'hrsh7th/cmp-nvim-lua' },     -- Optional
+      { 'hrsh7th/cmp-nvim-lua' }, -- Optional
 
       -- Snippets
-      { 'L3MON4D3/LuaSnip' },             -- Required
+      { 'L3MON4D3/LuaSnip' }, -- Required
       { 'rafamadriz/friendly-snippets' }, -- Optional
     }
   }
