f43b9b2fa9c0ff039a17baedc6a6d983a42d6b52
Alex Serheyev <alex@dna.tech>
2023-05-31 15:40:29 +0300

    ✨feat(nvim): add codeium plugin free copilot alternative

--- nvim/lua/sim/packer.lua
+++ nvim/lua/sim/packer.lua
@@ -88,4 +88,15 @@ return require("packer").startup(function(use)
       "nvim-telescope/telescope.nvim"
     }
   })
+
+  use {
+    'Exafunction/codeium.vim',
+    -- config = function()
+    --   -- Change '<C-g>' here to any keycode you like.
+    --   vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true })
+    --   vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+    --   vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+    --   vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+    -- end
+  }
 end)
