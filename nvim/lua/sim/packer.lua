666484c0de0efba956690509df9255a4c92c671d
Alex Serheyev <alex@dna.tech>
2023-08-10 18:46:24 +0300

    🎨format

--- nvim/lua/sim/packer.lua
+++ nvim/lua/sim/packer.lua
@@ -108,8 +108,8 @@ return require("packer").startup(function(use)
     'Exafunction/codeium.vim',
     config = function()
       vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true })
-      vim.keymap.set('i', '<C-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
-      vim.keymap.set('i', '<C-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+      vim.keymap.set('i', '<C-n>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+      vim.keymap.set('i', '<C-p>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
       vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
     end
   }
