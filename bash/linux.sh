ec70d5ca360a1b0df58bc2694fe45f5287410574
Alex Serheyev <alex@dna.tech>
2023-09-02 15:43:08 +0300

    🐛fix(bash): ss - new replacement netstat

--- bash/linux.sh
+++ bash/linux.sh
@@ -1,9 +1,9 @@
 alias youtube-dl='youtube-dl --format bestvideo+bestaudio[ext=mp4]/bestvideo+bestaudio/best'
 
-alias ss='sudo systemctl start'
-alias sss='sudo systemctl status'
-alias sst='sudo systemctl stop'
-alias sse='sudo systemctl enable'
-alias ssd='sudo systemctl disable'
+alias sys='sudo systemctl start'
+alias syss='sudo systemctl status'
+alias syst='sudo systemctl stop'
+alias syse='sudo systemctl enable'
+alias sysd='sudo systemctl disable'
 
 alias kitty-ssh="kitty +kitten ssh"
