d935fc4782995b6c2afdea3d7c1d30273bcdd382
Alex Serheyev <alex@dna.tech>
2024-01-20 21:20:29 +0200

    ➕feat(php): return composer

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -10,7 +10,7 @@ source $HOME/dotfiles/bash/common.sh
 source $HOME/dotfiles/bash/docker.sh
 source $HOME/dotfiles/bash/git.sh
 source $HOME/dotfiles/bash/golang.sh
-# source $HOME/dotfiles/bash/composer.sh
+source $HOME/dotfiles/bash/composer.sh
 # source $HOME/dotfiles/bash/symfony.sh
 source $HOME/dotfiles/bash/tmux.sh
 source $HOME/dotfiles/bash/ansible.sh
