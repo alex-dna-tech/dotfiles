b38746853ae8bd6daff3e7b3bd6c1f7784f9859a
Alex Serheyev <74361701+alex-dna-tech@users.noreply.github.com>
2024-06-13 16:06:58 +0300

    🩹fix(nvim): split usage of keymaps

--- nvim/after/plugin/cmdline.lua
+++ nvim/after/plugin/cmdline.lua
@@ -1,4 +1,3 @@
 require("telescope").load_extension('cmdline')
 
-vim.api.nvim_set_keymap('n', ':', ':Telescope cmdline<CR>', { noremap = true, desc = "Cmdline" })
 vim.api.nvim_set_keymap('n', '<leader><leader>', ':Telescope cmdline<CR>', { noremap = true, desc = "Cmdline" })
