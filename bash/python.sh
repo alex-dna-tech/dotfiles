e8afd9c2ea4a5b379f86b2db2ec9f436c5d61aa8
Alex Serheyev <74361701+alex-dna-tech@users.noreply.github.com>
2024-07-05 18:21:01 +0300

    ✨feat(bash): python start project

--- bash/python.sh
+++ bash/python.sh
@@ -16,6 +16,21 @@ function pe ()
   fi
 }
 
+pmd () {
+  cat << 'EOF' > "${1:-main.py}"
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def main():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
+EOF
+  echo "python-dotenv" > requirements.txt
+}
+
 alias jl="jupyter lab"
 
 ipkn ()
