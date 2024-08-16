f15cfde74c110c66e96d6fe62e88fc8113afc268
Alex Serheyev <74361701+alex-dna-tech@users.noreply.github.com>
2024-08-17 01:19:30 +0300

    🩹fix(nvim): add noice keymaps

--- nvim/lua/user/plugins/noice.lua
+++ nvim/lua/user/plugins/noice.lua
@@ -1,8 +1,10 @@
 return {
 	"folke/noice.nvim",
+	event = "VeryLazy",
 	dependencies = {
 		"MunifTanjim/nui.nvim",
 		"rcarriga/nvim-notify",
+		"nvim-telescope/telescope.nvim",
 	},
 	config = function()
 		---@diagnostic disable-next-line: missing-fields
@@ -30,5 +32,19 @@ return {
 				},
 			},
 		})
+
+		local k = vim.keymap
+		k.set("n", "<leader>nl", function()
+			require("noice").cmd("last")
+		end, { desc = "Noice Last Message" })
+
+		k.set("n", "<leader>nh", function()
+			require("noice").cmd("history")
+		end, { desc = "Noice Message History" })
+
+		require("telescope").load_extension("noice")
+		k.set("n", "<leader>nt", function()
+			require("noice").cmd("telescope")
+		end, { desc = "Noice Telescope Message" })
 	end,
 }
