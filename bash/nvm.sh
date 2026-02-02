9ea45a1018900f22162343b6287021db7651f2cb
Alex Serheyev <74361701+alex-dna-tech@users.noreply.github.com>
2026-02-02 20:05:58 +0200

    🩹fix(plugins): lazy load nvm

--- bash/nvm.sh
+++ bash/nvm.sh
@@ -1,39 +1,49 @@
-lazy_load_nvm() {
-  unset -f node nvm npm npx
-  export NVM_DIR=~/.nvm
-  [[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
-  [[ -s "$NVM_DIR/bash_completion" ]] && \. "$NVM_DIR/bash_completion" 
-}
-
-npm() {
-  lazy_load_nvm
-  command npm "$@"
-}
-
-npx() {
-  lazy_load_nvm
-  command npx "$@"
-}
-
-node() {
-  lazy_load_nvm
-  command node "$@"
-}
-
-tm() {
-  lazy_load_nvm
-  task-master "$@"
-}
-
-nvm() {
-  lazy_load_nvm
-  nvm "$@"
-}
-
+unset -f node nvm npm npx
+export NVM_DIR=~/.nvm
+[[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
 if [[ -x "$(command -v nvim)" ]]; then
   v() {
-    lazy_load_nvm
     command nvim "$@"
   }
 fi
+# [[ -s "$NVM_DIR/bash_completion" ]] && \. "$NVM_DIR/bash_completion" 
 
+# lazy_load_nvm() {
+#   unset -f node nvm npm npx
+#   export NVM_DIR=~/.nvm
+#   [[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
+#   [[ -s "$NVM_DIR/bash_completion" ]] && \. "$NVM_DIR/bash_completion" 
+# }
+#
+# npm() {
+#   lazy_load_nvm
+#   command npm "$@"
+# }
+#
+# npx() {
+#   lazy_load_nvm
+#   command npx "$@"
+# }
+#
+# node() {
+#   lazy_load_nvm
+#   command node "$@"
+# }
+#
+# tm() {
+#   lazy_load_nvm
+#   task-master "$@"
+# }
+#
+# nvm() {
+#   lazy_load_nvm
+#   nvm "$@"
+# }
+#
+# if [[ -x "$(command -v nvim)" ]]; then
+#   v() {
+#     lazy_load_nvm
+#     command nvim "$@"
+#   }
+# fi
+#
