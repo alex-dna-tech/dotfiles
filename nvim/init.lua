694e9733e6614e9b87e2375ca8c3c8447016f452
Alex Serheyev <74361701+alex-dna-tech@users.noreply.github.com>
2026-02-28 08:34:46 +0200

    💄fix(alacritty): transparent background

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,3 +9,7 @@ vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
 	pattern = "*",
 	command = "silent! wall",
 })
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
