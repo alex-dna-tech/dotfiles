d239e28e8135649f23e7aa63878ac93d5463e635
Alex Serheyev <74361701+alex-dna-tech@users.noreply.github.com>
2026-08-09 14:44:06 +0300

    fix(plugins): oil hot key fix

--- nvim/lua/user/plugins/oil.lua
+++ nvim/lua/user/plugins/oil.lua
@@ -1,11 +1,15 @@
 return {
 	"stevearc/oil.nvim",
 	lazy = false,
-	opts = {
-		default_file_explorer = true,
-		keymaps = {
-			["<C-r>"] = "actions.refresh",
-		},
-	},
+	config = function()
+		require("oil").setup({
+			default_file_explorer = true,
+			keymaps = {
+				["<C-r>"] = "actions.refresh",
+			},
+		})
+		local k = vim.keymap
+		k.set("n", "<leader>o", "<cmd>Oil<cr>", { desc = "Files Explorer", nowait = true })
+	end,
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 }
