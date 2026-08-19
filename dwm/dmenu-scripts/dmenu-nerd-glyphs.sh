13fdf8d42b701fd9c232cf8055fb3d4d45745d55
Alex Serheyev <74361701+alex-dna-tech@users.noreply.github.com>
2026-08-19 03:34:04 +0300

    dmenu: rewrite glyph picker to use nerd-fonts glyphnames.json

--- dwm/dmenu-scripts/dmenu-nerd-glyphs.sh
+++ dwm/dmenu-scripts/dmenu-nerd-glyphs.sh
@@ -5,26 +5,20 @@ GLYPH_FILE="${CACHE_DIR}/glyph-list.txt"
 
 mkdir -p "$CACHE_DIR"
 
-# Get user selection via dmenu from emoji file.
-if [ -e "$GLYPH_FILE" ]
-then
-    chosen=$(cut -d ';' -f1 "$GLYPH_FILE" | dmenu -i -l 30 | sed "s/ .*//")
-else
-    echo -en "$(curl https://www.nerdfonts.com/cheat-sheet \
-        | grep 'class="class-name">' \
-        | sed 's/ *<div class="class-name">//; s/<\/div><div class="codepoint">/ \\u/; s/<\/div>//; s/\(.* \)\(.*\)/\2 \1/' \
-        )" > "$GLYPH_FILE"
-    chosen=$(cut -d ';' -f1 "$GLYPH_FILE" | dmenu -i -l 30 | sed "s/ .*//")
+if [ ! -e "$GLYPH_FILE" ]; then
+    curl -s https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/glyphnames.json \
+        | sed 's/},/}\n/g' \
+        | grep '"char"' \
+        | sed 's/^"\([^"]*\)":{"char":"\([^"]*\)".*/\1 \2/' \
+        > "$GLYPH_FILE"
 fi
 
-# Exit if none chosen.
+chosen=$(dmenu -i -l 30 < "$GLYPH_FILE" | cut -d ' ' -f2)
 [ -z "$chosen" ] && exit
 
-# If you run this command with an argument, it will automatically insert the
-# character. Otherwise, show a message that the emoji has been copied.
 if [ -n "$1" ]; then
-	xdotool type "$chosen"
+    xdotool type "$chosen"
 else
-	printf "$chosen" | xclip -selection clipboard
-	notify-send "'$chosen' copied to clipboard." &
+    printf '%s' "$chosen" | xclip -selection clipboard
+    notify-send "'$chosen' copied to clipboard." &
 fi
