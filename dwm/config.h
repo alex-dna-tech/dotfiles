69dbe603315d9f5f1639b91afeedeff38f30b7cc
Alex Serheyev <74361701+alex-dna-tech@users.noreply.github.com>
2026-08-20 20:35:44 +0300

    fix(dwm): add dmenu keys

--- dwm/config.h
+++ dwm/config.h
@@ -62,7 +62,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-bw", 5, "-g", 4, "-l", 6, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *flameshot[] = { "flameshot", "gui", NULL };
 static const char *picom_on[] = { "picom", NULL };
