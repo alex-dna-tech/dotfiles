987fcc039b2538f4b02da1d7bf97509b7dfbbd28
Alex Serheyev <74361701+alex-dna-tech@users.noreply.github.com>
2026-08-21 16:43:17 +0300

    fix(dwm): dunst, dmenu config

--- dwm/config.h
+++ dwm/config.h
@@ -11,7 +11,7 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "FiraCode Nerd Font:style:medium:size=12", "FiraCode Nerd Font Mono:style:medium:size=19" };
-static const char dmenufont[]       = "FiraCode Nerd Font Mono:size=20:style=Bold";
+static const char dmenufont[]       = "FiraCode Nerd Font Mono:size=14:style=Bold";
 static const char col_gray1[]       = "#282822"; /* gruvbox bg dark */
 static const char col_gray2[]       = "#3c3836"; /* gruvbox bg1 */
 static const char col_gray3[]       = "#ebdbb2"; /* gruvbox fg */
@@ -62,7 +62,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-bw", "5", "-g", "4", "-l", "6", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-bw", "5", "-g", "2", "-l", "6", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *flameshot[] = { "flameshot", "gui", NULL };
 static const char *picom_on[] = { "picom", NULL };
