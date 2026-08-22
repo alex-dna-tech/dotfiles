145b9dc6eb947cf5ce62d6ab71baba00a9af669f
Alex Serheyev <74361701+alex-dna-tech@users.noreply.github.com>
2026-08-22 21:24:44 +0300

    🩹fix(bash): linux conf

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,4 +1,4 @@
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+if [[ "$OSTYPE" == "linux"* ]]; then
   source $HOME/dotfiles/bash/linux.sh
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   source $HOME/dotfiles/bash/macos.sh
--- bash/linux.sh
+++ bash/linux.sh
@@ -1,9 +1,13 @@
 alias youtube-dl='youtube-dl --format bestvideo+bestaudio[ext=mp4]/bestvideo+bestaudio/best'
 
-alias sys='sudo systemctl start'
-alias syss='sudo systemctl status'
-alias syst='sudo systemctl stop'
-alias syse='sudo systemctl enable'
-alias sysd='sudo systemctl disable'
 
-alias kitty-ssh="kitty +kitten ssh"
+if [ -f /etc/alpine-release ]; then
+  alias rs='sudo rc-service'
+  alias ru='sudo rc-update'
+else
+  alias sys='sudo systemctl start'
+  alias syss='sudo systemctl status'
+  alias syst='sudo systemctl stop'
+  alias syse='sudo systemctl enable'
+  alias sysd='sudo systemctl disable'
+fi
