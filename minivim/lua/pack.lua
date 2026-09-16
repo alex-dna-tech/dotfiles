63d1380142ab706c710eb9d9e0f892037ff7d4b8
Alex Serheyev <74361701+alex-dna-tech@users.noreply.github.com>
2026-09-16 13:34:01 +0300

    fix(nvim): create folder first

--- minivim/lua/pack.lua
+++ minivim/lua/pack.lua
@@ -1,55 +1,71 @@
 vim.pack.add({
-    { src = "https://github.com/ellisonleao/gruvbox.nvim" },
-    { src = "https://github.com/nvim-mini/mini.nvim" },
-    { src = "https://github.com/rafamadriz/friendly-snippets" },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter", branch = "main" },
-    { src = "https://github.com/neovim/nvim-lspconfig" },
-    { src = "https://github.com/mason-org/mason.nvim" },
-    { src = "https://github.com/tpope/vim-fugitive" },
-    { src = "https://github.com/tpope/vim-rhubarb" },
+  { src = "https://github.com/ellisonleao/gruvbox.nvim" },
+  { src = "https://github.com/nvim-mini/mini.nvim" },
+  { src = "https://github.com/rafamadriz/friendly-snippets" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter", branch = "main" },
+  { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/mason-org/mason.nvim" },
+  { src = "https://github.com/tpope/vim-fugitive" },
+  { src = "https://github.com/tpope/vim-rhubarb" },
 })
 
 vim.cmd.colorscheme("gruvbox")
 
 -- mini files ----
 local MiniFiles = require("mini.files")
-MiniFiles.setup()
+MiniFiles.setup({
+  mappings = {
+    close       = '<ESC>',
+    go_in       = 'l',
+    go_in_plus  = '<CR>',
+    go_out      = 'h',
+    go_out_plus = 'H',
+    mark_goto   = "'",
+    mark_set    = 'm',
+    reset       = '<BS>',
+    reveal_cwd  = '@',
+    show_help   = 'g?',
+    synchronize = '=',
+    trim_left   = '<',
+    trim_right  = '>',
+  },
+})
 
 vim.keymap.set("n", "-", MiniFiles.open, { desc = "Toggle mini file explorer" })
 vim.keymap.set("n", "<leader>o", function()
-    MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
-    MiniFiles.reveal_cwd()
+  MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+  MiniFiles.reveal_cwd()
 end, { desc = "Toggle into currently opened file" })
 
 ---- mini notify ----
 require("mini.notify").setup({
-    -- only show messages
-    content = {
-        format = function(notif)
-            return notif.msg
-        end,
-    },
+  -- only show messages
+  content = {
+    format = function(notif)
+      return notif.msg
+    end,
+  },
 })
 
 local MiniSnippets = require("mini.snippets")
 MiniSnippets.setup({
-    snippets = {
-        MiniSnippets.gen_loader.from_lang(), -- loads friendly-snippets
-    },
+  snippets = {
+    MiniSnippets.gen_loader.from_lang(),     -- loads friendly-snippets
+  },
 
-    mappings = {
-        expand = '<C-j>',
-        jump_next = '<TAB>',
-        jump_prev = '<S-TAB>',
-        stop = '<C-c>',
-    },
+  mappings = {
+    expand = '<C-j>',
+    jump_next = '<TAB>',
+    jump_prev = '<S-TAB>',
+    stop = '<C-c>',
+  },
 
-    expand = {
-        prepare = nil,
-        match = nil,
-        select = nil,
-        insert = nil,
-    },
+  expand = {
+    prepare = nil,
+    match = nil,
+    select = nil,
+    insert = nil,
+  },
 })
 
 MiniSnippets.start_lsp_server({ match = false })
@@ -57,13 +73,13 @@ MiniSnippets.start_lsp_server({ match = false })
 require('mini.pairs').setup()
 require('mini.splitjoin').setup()
 require("mini.surround").setup({
-mappings = {
-    add = 'as', -- Add surrounding in Normal and Visual modes
-    delete = 'ds', -- Delete surrounding
-    find = 'fs', -- Find surrounding (to the right)
-    find_left = 'Fs', -- Find surrounding (to the left)
-    highlight = 'hs', -- Highlight surrounding
-    replace = 'cs', -- Replace surrounding
+  mappings = {
+    add = 'as',        -- Add surrounding in Normal and Visual modes
+    delete = 'ds',     -- Delete surrounding
+    find = 'fs',       -- Find surrounding (to the right)
+    find_left = 'Fs',  -- Find surrounding (to the left)
+    highlight = 'hs',  -- Highlight surrounding
+    replace = 'cs',    -- Replace surrounding
 
     suffix_last = 'l', -- Suffix to search with "prev" method
     suffix_next = 'n', -- Suffix to search with "next" method
@@ -72,7 +88,7 @@ mappings = {
 
 --- mini cmdline completion ---
 require("mini.cmdline").setup({
-    autocorrect = { enable = false }
+  autocorrect = { enable = false }
 })
 require("mini.icons").setup()
 require('mini.statusline').setup()
@@ -98,8 +114,10 @@ MiniExtra.setup()
 -- keymaps
 vim.keymap.set("n", "<leader>f", "", { desc = "+Files" })
 vim.keymap.set("n", "<leader>ff", MiniPick.builtin.files, { desc = "Mini File Picker" })
-vim.keymap.set("n", "<leader>fs", function() MiniPick.builtin.grep_live({ local_opts = { tool = 'rg' } }) end, { desc = "Search in files" })
-vim.keymap.set("n", "<leader>fw", function() MiniPick.builtin.grep({ pattern = vim.fn.expand("<cword>") }) end, { desc = "Search word" })
+vim.keymap.set("n", "<leader>fs", function() MiniPick.builtin.grep_live({ local_opts = { tool = 'rg' } }) end,
+  { desc = "Search in files" })
+vim.keymap.set("n", "<leader>fw", function() MiniPick.builtin.grep({ pattern = vim.fn.expand("<cword>") }) end,
+  { desc = "Search word" })
 
 vim.keymap.set("n", "<leader>s", "", { desc = "+Search" })
 vim.keymap.set("n", "<leader>sh", MiniPick.builtin.help, { desc = "Mini Help" })
@@ -111,16 +129,16 @@ vim.keymap.set("n", "<leader>b", MiniPick.builtin.buffers, { desc = "Mini Picker
 
 --- mini completions ---
 require("mini.completion").setup({
-    lsp_completion = {
-        auto_setup = true,
-    }
+  lsp_completion = {
+    auto_setup = true,
+  }
 })
 
 
 --- mini diff and fugitive ---
 local MiniDiff = require("mini.diff")
 MiniDiff.setup({
-    source = MiniDiff.gen_source.git({ index = false }),
+  source = MiniDiff.gen_source.git({ index = false }),
 })
 
 vim.keymap.set("n", "<leader>g", "", { desc = "+Git" })
@@ -148,10 +166,10 @@ local MiniHipatterns = require('mini.hipatterns')
 MiniHipatterns.setup({
   highlighters = {
     -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
-    fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
-    hack  = { pattern = '%f[%w]()HACK()%f[%W]',  group = 'MiniHipatternsHack'  },
-    todo  = { pattern = '%f[%w]()TODO()%f[%W]',  group = 'MiniHipatternsTodo'  },
-    note  = { pattern = '%f[%w]()NOTE()%f[%W]',  group = 'MiniHipatternsNote'  },
+    fixme     = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+    hack      = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
+    todo      = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
+    note      = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
 
     -- Highlight hex color strings (`#rrggbb`) using that color
     hex_color = MiniHipatterns.gen_highlighter.hex_color(),
